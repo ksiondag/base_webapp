e1b6fc4a4ea68998ce35e7cdbed0e425af8f30d4
Rory Ksionda <ksiondag846@gmail.com>
2020-01-25 07:23:36 -0800

    React router with 404 login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 
@@ -16,7 +17,9 @@ function App() {
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight>
-                        <NavItem href="/login">Login</NavItem>
+                        <LinkContainer to="/login">
+                            <NavItem>Login</NavItem>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
