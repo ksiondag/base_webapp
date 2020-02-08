7137871eaafdd9c51068aeb9e0a1faeae08ec656
Rory Ksionda <ksiondag846@gmail.com>
2020-02-08 15:49:43 -0800

    Slight update

--- src/containers/funds/AddFund.tsx
+++ src/containers/funds/AddFund.tsx
@@ -10,28 +10,34 @@ export default function AddFund(props: React.PropsWithChildren<AddFundProps>) {
     const [fundName, setFundName] = React.useState("");
     const [balance, setBalance] = React.useState(0.00);
 
+    const handleSubmit = () => props.saveFund(fundName, balance);
+
     return (
         <tr key="add">
             <td>
-                <FormControl
-                    autoFocus
-                    value={fundName}
-                    type="text"
-                    placeholder="Fund name"
-                    onChange={e => setFundName((e.target as HTMLTextAreaElement).value)}
-                />
+                <form onSubmit={handleSubmit}>
+                    <FormControl
+                        autoFocus
+                        value={fundName}
+                        type="text"
+                        placeholder="Fund name"
+                        onChange={e => setFundName((e.target as HTMLTextAreaElement).value)}
+                    />
+                </form>
             </td>
             <td>
-                <FormControl
-                    type="text"
-                    value={balance}
-                    placeholder="0.00"
-                    onChange={e => setBalance(parseFloat((e.target as HTMLTextAreaElement).value))}
-                />
+                <form onSubmit={handleSubmit}>
+                    <FormControl
+                        type="text"
+                        value={balance}
+                        placeholder="0.00"
+                        onChange={e => setBalance(parseFloat((e.target as HTMLTextAreaElement).value))}
+                    />
+                </form>
             </td>
             <td>
                 <ButtonToolbar>
-                    <Button onClick={() => props.saveFund(fundName, balance)}>Save</Button>
+                    <Button onClick={handleSubmit}>Save</Button>
                     <Button onClick={() => props.setAddFund(false)}>Cancel</Button>
                 </ButtonToolbar>
             </td>
