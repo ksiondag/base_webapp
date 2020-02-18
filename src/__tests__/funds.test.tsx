33da85bad8f6874e4ad4988271d854cfe2f94b0f
Rory Ksionda <ksiondag846@gmail.com>
2020-02-17 19:34:13 -0800

    Better test that doesn't actually test anything

--- src/__tests__/funds.test.tsx
+++ src/__tests__/funds.test.tsx
@@ -1,10 +1,44 @@
 import * as React from "react";
-import renderer from 'react-test-renderer';
+import { MemoryRouter } from "react-router-dom";
+import { render, wait } from "@testing-library/react";
+
+import fundsApi from "../api/funds";
+import AppliedRoute from "../components/AppliedRoute";
+import Funds from "../containers/Funds";
+
+jest.mock("../api/funds");
+jest.mock("../api/token");
+
+const mockFundsApi = fundsApi as jest.Mocked<typeof fundsApi>;
+
+afterEach(() => {
+    jest.resetAllMocks()
+})
 
 
-test('Funds render as a table of names and balances', () => {
-    const component = renderer.create(
-        <div>
-        </div>
+test('Funds render as a table of names and balances', async () => {
+    const appProps = {
+        isAuthenticated: false,
+        userHasAuthenticated: (): null => null,
+    };
+
+    mockFundsApi.get.mockResolvedValue({
+        success: true,
+        funds: [
+            //{ "id": 11, "name": "Woo", "balance": 0, "balanceDate": "2020-02-08", "userId": 1 },
+            //{ "id": 12, "name": "Another", "balance": 0, "balanceDate": "2020-02-08", "userId": 1 },
+            //{ "id": 13, "name": "Oh really?", "balance": 250, "balanceDate": "2020-02-08", "userId": 1 },
+            //{ "id": 14, "name": "Nick", "balance": 100000, "balanceDate": "2020-02-09", "userId": 1 },
+            //{ "id": 15, "name": "type a thing", "balance": 0, "balanceDate": "2020-02-09", "userId": 1 },
+            //{ "id": 16, "name": "yas", "balance": 250, "balanceDate": "2020-02-09", "userId": 1 }
+        ]
+    })
+
+    render(
+        <MemoryRouter>
+            <AppliedRoute component={Funds} appProps={appProps} />
+        </MemoryRouter>
     );
+
+    await wait();
 });
