3b055079a459809652d6ef8fca525763b51662d4
Rory Ksionda <ksiondag846@gmail.com>
2020-02-17 16:29:51 -0800

    Testing skeleton

---
+++ src/__tests__/funds.test.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import renderer from 'react-test-renderer';
+
+
+test('Funds render as a table of names and balances', () => {
+    const component = renderer.create(
+        <div>
+        </div>
+    );
+});
